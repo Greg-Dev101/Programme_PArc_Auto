7b1d24e7bd7a17af0dc92050eb2ee1d15ca56b8d
Grégory Ysewyn <Gregoryysewyn@macbook-de-gregory-ysewyn.home>
2020-12-02 23:58:07 +0100

    3 method ok

--- color.js
+++
@@ -1,9 +0,0 @@
-class Color {
-    constructor(){
-        console.log ("nouvelle création")
-    }
-}
-
-const red = new Color()
-const green = new Color()
-const yellow = new Color()
--- index.js
+++ index.js
@@ -1,32 +1,92 @@
 
-class Voiture {
-    constructor(immat, couleur, poids, puissance, capReserve, nivEssence, nbrePlace){
+class Car {
+    constructor(immat, color, weight, power, tankcapacity, fuelQuantity, seats, assurance, bordMessage){
     this.immat = immat;
-    this.couleur = couleur;
-    this.poids = poids;
-    this.puissance = puissance;
-    this.capReserve = capReserve;
-    this.nivEssence = nivEssence;
-    this.nbrePlace = nbrePlace;
+    this.color = color;
+    this.weight = weight;
+    this.power = power;
+    this.tankcapacity = tankcapacity;
+    this.fuelQuantity = fuelQuantity;
+    this.seats = seats
+    this.assurance = false;
+    this.bordMessage = "Bienvenue";
     }
     
-    repeindre(nouvelleCouleur) {
-        if (nouvelleCouleur === this.couleur) {
+    repeindre(newColor) {
+        if (newColor === this.color) {
         console.log('Couleur identique');
-        } else {
-        this.couleur = nouvelleCouleur;
-        console.log('La nouvelle couleur est : ' + this.couleur);
+        } 
+        else {
+        this.color = newColor;
+        console.log('La nouvelle couleur est : ' + this.color);
+        }
+    }
+
+    mettreEssence(newFuelQuantity) {
+        if ((newFuelQuantity+this.fuelQuantity) <= this.tankcapacity) {
+            console.log("Appoint d'essence effectué, vous disposez désormais de " + (newFuelQuantity+this.fuelQuantity) + " litres d'essence." )
+            this.fuelQuantity = (newFuelQuantity+this.fuelQuantity)
+        }
+        else {
+            console.log("Appoint impossible, veuillez recommencer")
+        }
+    }
+    
+    sedeplacer(distance, speed) {
+        
+        if ((speed<=50)) {
+        let consumption=distance*0.1
+            if ((consumption>=this.fuelQuantity)) {
+                console.log ("Trajet impossible")
+            }
+            else {
+                console.log("Vous avez consommé " + consumption + " litres. Il vous reste " + (this.fuelQuantity-consumption) + " litres.")
+            }
+        }
+              
+        if ((speed>50) && (speed<=90)) {
+        let consumption=distance*0.05
+            if ((consumption>=this.fuelQuantity)) {
+                console.log ("Trajet impossible")
+            }
+            else {
+                console.log("Vous avez consommé " + consumption + " litres. Il vous reste " + (this.fuelQuantity-consumption) + " litres.")
+            }
+        }
+
+        if ((speed>90) && (speed<=130)) {
+        let consumption=distance*0.08
+            if ((consumption>=this.fuelQuantity)) {
+                console.log ("Trajet impossible")
+            }
+            else {
+                console.log("Vous avez consommé " + consumption + " litres. Il vous reste " + (this.fuelQuantity-consumption) + " litres.")
+            }
+        }
+
+        if ((speed>130)) {
+            let consumption=distance*0.12
+            if ((consumption>=this.fuelQuantity)) {
+                console.log ("Trajet impossible")
+            }
+            else {
+                console.log("Vous avez consommé " + consumption + " litres. Il vous reste " + (this.fuelQuantity-consumption) + " litres.")
+            }
         }
     }
 }
 
-const voiture1 = new Voiture("AA 111 BB", "red", 1200, 6, 40.0, 5.0, 5)
-console.log(voiture1)
+const car1 = new Car("AA 111 BB", "red", 1200, 110, 40.0, 5.0, 5, false, "")
+console.log(car1)
 
-voiture1.repeindre("red")
-console.log(voiture1.immat + " " + voiture1.couleur)
+car1.repeindre("red")
 
+car1.repeindre("black")
 
+car1.mettreEssence(36)
+console.log(car1)
 
+car1.mettreEssence(34)
+console.log(car1)
 
-
+car1.sedeplacer(300, 120)
